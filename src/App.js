98e7e56998005471f8f746a93b38c3f7e6a37cce
carrielzada <offcarriel@gmail.com>
2024-05-19 21:58:05 -0300

    falta table, rotas e estilização, depois junção

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 import SideBar from './Componentes/SiderBar/SideBar';
-import Header from './Componentes/Header/Header';
 import FormColab from './Componentes/FormColab/FormColab';
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <SideBar/>
       <div className='container-fluid'>
-        <Header/>
         <div className='container'>
           <FormColab/>
         </div>
